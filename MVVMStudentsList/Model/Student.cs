870b1ec586356b1cff1dc6780149c50f037d333e
Rafał Okuniewski <rafal.okuniewski@gmail.com>
2016-11-19 02:40:48 +0100

    1

---
+++ MVVMStudentsList/Model/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MVVMStudentsList.Model
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Index { get; set; }
+        public string BirthPlace { get; set; }
+        public virtual Group Group { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
